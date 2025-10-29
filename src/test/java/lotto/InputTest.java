00ad59362c70291b284cfd1f2e1b8b00f3a7bf8b
KiSeungMin <ksm011212@gmail.com>
2025-10-29 22:55:54 +0900

    test(InputTest): 당첨번호 입력 기능에 대한 테스트 코드 작성

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -14,7 +14,7 @@ class InputTest extends NsTest{
     @Test
     void 정상_금액_입력() {
         assertSimpleTest(() -> {
-            run("8000");
+            run("8000", "1,2,3,4,5,6");
         });
     }
 
@@ -53,6 +53,54 @@ class InputTest extends NsTest{
     @Test
     void 예외_매우_큰_금액_입력 () {
         assertThatThrownBy(() -> run("20000000000000000"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]")
+                .hasNoCause();
+    }
+
+    @Test
+    void 예외_잘못된_당첨_번호_입력 () {
+        assertThatThrownBy(() -> run("1000", "1a,2,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]")
+                .hasNoCause();
+    }
+
+    @Test
+    void 예외_당첨_번호_개수_부족_입력 () {
+        assertThatThrownBy(() -> run("1000", "1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]")
+                .hasNoCause();
+    }
+
+    @Test
+    void 예외_당첨_번호_개수_초과_입력 () {
+        assertThatThrownBy(() -> run("1000", "1,2,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]")
+                .hasNoCause();
+    }
+
+    @Test
+    void 예외_당첨_번호_잘못된_범위_입력 () {
+        assertThatThrownBy(() -> run("1000", "-1,2,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]")
+                .hasNoCause();
+    }
+
+    @Test
+    void 예외_당첨_번호_소수_입력 () {
+        assertThatThrownBy(() -> run("1000", "1.8,2,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]")
+                .hasNoCause();
+    }
+
+    @Test
+    void 예외_당첨_번호_중복_숫자_입력 () {
+        assertThatThrownBy(() -> run("1000", "1,1,3,4,5,6"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]")
                 .hasNoCause();
