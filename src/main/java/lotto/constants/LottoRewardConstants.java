126a4703cd6ed82ba9680cf7de2c122e58581cc3
KiSeungMin <ksm011212@gmail.com>
2025-10-30 16:43:34 +0900

    feat: 로또 보상 관련 상수, 구조체 구현

---
+++ src/main/java/lotto/constants/LottoRewardConstants.java
@@ -0,0 +1,9 @@
+package lotto.constants;
+
+public class LottoRewardConstants {
+    public static final long FIRST_PRIZE_REWARD = 20000000000L;
+    public static final long SECOND_PRIZE_REWARD = 30000000L;
+    public static final long THIRD_PRIZE_REWARD = 1500000L;
+    public static final long FOURTH_PRIZE_REWARD = 50000L;
+    public static final long FIFTH_PRIZE_REWARD = 5000L;
+}
