243316adc7562d40619e7ef8d2970c4076704d13
KiSeungMin <ksm011212@gmail.com>
2025-10-30 16:44:01 +0900

    feat(LottoRewardInfo): 로또 보상 객체 구현

---
+++ src/main/java/lotto/lotto/LottoRewardInfo.java
@@ -0,0 +1,31 @@
+package lotto.lotto;
+
+import static lotto.constants.LottoConstants.REWARD_LIMIT;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoRewardInfo {
+    private List<Integer> rankCounts;
+
+    public LottoRewardInfo() {
+        this.rankCounts = new ArrayList();
+        for (int i = 0; i <= REWARD_LIMIT; i++) {
+            rankCounts.add(0);
+        }
+    }
+
+    public void addRankCount(Integer rank) {
+        Integer count = rankCounts.get(rank);
+        rankCounts.set(rank, count + 1);
+    }
+
+    public BigDecimal getTotalReward() {
+        BigDecimal totalReward = new BigDecimal(0);
+        for (int i = 1; i <= REWARD_LIMIT; i++) {
+            totalReward = totalReward.add(BigDecimal.valueOf(rankCounts.get(i) * Rank.values()[i].getPrize()));
+        }
+        return totalReward;
+    }
+}
