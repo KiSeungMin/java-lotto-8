a70f187015845fdbce3422147e2540079bc58486
KiSeungMin <ksm011212@gmail.com>
2025-10-30 18:33:40 +0900

    fix(InputTest): 입출력 예외 테스트가 assertSimpleTest를 사용하도록 수정

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -20,114 +20,122 @@ class InputTest extends NsTest{
 
     @Test
     void 예외_1000원_단위가_아닌_금액_입력 () {
-        assertThatThrownBy(() -> run("8500"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("8500");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_1000원_미만_금액_입력 () {
-        assertThatThrownBy(() -> run("700"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("700");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_음수_금액_입력 () {
-        assertThatThrownBy(() -> run("-1000"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("-1000");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_0원_금액_입력 () {
-        assertThatThrownBy(() -> run("0"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("0");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_매우_큰_금액_입력 () {
-        assertThatThrownBy(() -> run("20000000000000000"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("20000000000000000");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_잘못된_형태_금액_입력 () {
+        assertSimpleTest(() -> {
+            runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_잘못된_당첨_번호_입력 () {
-        assertThatThrownBy(() -> run("1000", "1a,2,3,4,5,6"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("1000", "1a,2,3,4,5,6");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_당첨_번호_개수_부족_입력 () {
-        assertThatThrownBy(() -> run("1000", "1,2,3,4,5"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_당첨_번호_개수_초과_입력 () {
-        assertThatThrownBy(() -> run("1000", "1,2,3,4,5,6,7"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6,7");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_당첨_번호_잘못된_범위_입력 () {
-        assertThatThrownBy(() -> run("1000", "-1,2,3,4,5,6"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("1000", "-1,2,3,4,5,6");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_당첨_번호_소수_입력 () {
-        assertThatThrownBy(() -> run("1000", "1.8,2,3,4,5,6"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("1000", "1.8,2,3,4,5,6");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_당첨_번호_중복_숫자_입력 () {
-        assertThatThrownBy(() -> run("1000", "1,1,3,4,5,6"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("1000", "1,1,3,4,5,6");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_보너스_번호_잘못된_범위_입력 () {
-        assertThatThrownBy(() -> run("1000", "1,2,3,4,5,6", "-1"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "-1");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_보너스_번호_소수_입력 () {
-        assertThatThrownBy(() -> run("1000", "1,2,3,4,5,6", "3.5"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "3.5");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Test
     void 예외_보너스_번호_중복_입력 () {
-        assertThatThrownBy(() -> run("1000", "1,2,3,4,5,6", "3"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]")
-                .hasNoCause();
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "3");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Override
